48ca484a51dd40fbabaadfc8cb0cc6a538a3aa6d
Killassa <greg12876@gmail.com>
2022-11-10 17:29:06 +0100

    Implement MailGenerator

--- src/main/java/org/example/MailGenerator.java
+++ src/main/java/org/example/MailGenerator.java
@@ -1,26 +1,58 @@
 package org.example;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class MailGenerator {
-    List<String> emails;
-    List<String> messages;
+    private final LinkedList<String> emails;
+    private final LinkedList<String> messages;
+    private final int nbGroup;
 
-    public MailGenerator(List<String> emails, List<String> messages) {
+    private final Random rd;
+
+    public MailGenerator(LinkedList<String> emails, LinkedList<String> messages, int nbGroup) {
         this.emails = emails;
         this.messages = messages;
+        this.nbGroup = nbGroup;
+        rd = new Random();
     }
 
-    public List<Objects> generateMail() {
-        return null;
+    public LinkedList<Mail> generateMail() {
+        LinkedList<Mail> mails = new LinkedList<>();
+        LinkedList<LinkedList<String>> groups = getRandomGroups();
+        LinkedList<String> groupsMessages = getRandomMessages();
+
+        for (int i = 0; i < nbGroup; i++) {
+            int idxSender = rd.nextInt(emails.size());
+            String sender = groups.get(i).get(idxSender);
+            groups.get(i).remove(idxSender);
+
+            mails.add(new Mail(/*sender, groups.get(i), groupsMessages.get(i)*/));
+        }
+
+        return mails;
     }
 
-    private List<List<String>> getRandomGroup(int nbGroup) {
-        return null;
+    private LinkedList<LinkedList<String>> getRandomGroups() {
+        LinkedList<LinkedList<String>> groups = new LinkedList<>();
+        for (int i = 0; i < nbGroup; i++) {
+            groups.add(new LinkedList<>());
+            int idxEmails = rd.nextInt(emails.size());
+            groups.get(i).add(emails.get(idxEmails));
+            emails.remove(idxEmails);
+        }
+
+        return groups;
     }
 
-    private String getRandomMessage() {
-        return "";
+    private LinkedList<String> getRandomMessages() {
+        LinkedList<String> groupsMessages = new LinkedList<>();
+        for (int i = 0; i < nbGroup; i++) {
+            int idxMessages = rd.nextInt(messages.size());
+            messages.add(messages.get(idxMessages));
+            emails.remove(idxMessages);
+        }
+
+        return groupsMessages;
     }
 }
