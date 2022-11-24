5bebdcaccbac5b91657b0dfd3e08bba273828dc7
Killassa <gregory.reymermet@gmail.com>
2022-11-24 11:34:18 +0100

    Add comment SmtpConfig.java

--- src/main/java/org/example/SmtpConfig.java
+++ src/main/java/org/example/SmtpConfig.java
@@ -17,19 +17,47 @@ import java.util.Properties;
 import java.util.regex.Pattern;
 
 public class SmtpConfig {
+    /**
+     * Emplacement des ressources
+     */
     private final static String RESSOURCES_PATH = "resources/";
+
+    /**
+     * Pattern regex pour check le format de l'email
+     */
     private final static Pattern PATTERN = Pattern.compile("^[A-Za-z0-9._%+-]*@[A-Za-z0-9.-]*\\.[A-Za-z]*$");
 
+    /**
+     * Fichier contenant les configurations
+     */
     private final String configFile;
+
+    /**
+     * Fichier contenant les emails
+     */
     private final String emailsFile;
+
+    /**
+     * Fichier contenant les messages
+     */
     private final String messagesFile;
 
+    /**
+     * Constructeur d'un SmtpConfig
+     * @param configFile    Fichier contenant les configurations
+     * @param emailsFile    Fichier contenant les emails
+     * @param messagesFile  Fichier contenant les messages
+     */
     public SmtpConfig(String configFile, String emailsFile, String messagesFile) {
         this.configFile = configFile;
         this.emailsFile = emailsFile;
         this.messagesFile = messagesFile;
     }
 
+    /**
+     * Lis les emails depuis le fichier de ressource
+     * @return La liste de tous les emails
+     */
     public LinkedList<String> getEmails() {
         LinkedList<String> emails = new LinkedList<>();
 
@@ -51,6 +79,10 @@ public class SmtpConfig {
         return emails;
     }
 
+    /**
+     * Lis les messages depuis le fichier de ressource
+     * @return La liste de tous les messages
+     */
     public LinkedList<Message> getMessages() {
         LinkedList<Message> messages = new LinkedList<>();
 
@@ -80,6 +112,10 @@ public class SmtpConfig {
         return messages;
     }
 
+    /**
+     * Obtiens les propriétés depuis le fichier de configuration
+     * @return Un objet contenant les propriétés
+     */
     private Properties getProps() {
         Properties props = new Properties();
 
@@ -92,10 +128,18 @@ public class SmtpConfig {
         return props;
     }
 
+    /**
+     * Obtenir l'adresse du serveur
+     * @return Adresse du serveur
+     */
     public String getServerAddress() {
         return getProps().getProperty("smtpServerAddress", "localhost");
     }
 
+    /**
+     * Obtenir le port du serveur
+     * @return Port du serveur
+     */
     public int getServerPort() {
         try {
             return Integer.parseInt(getProps().getProperty("smtpServerPort", "25"));
@@ -104,6 +148,10 @@ public class SmtpConfig {
         }
     }
 
+    /**
+     * Obtenir le nombre de groupe
+     * @return Nombre de groupe
+     */
     public int getNumberOfGroups() {
         try {
             return Integer.parseInt(getProps().getProperty("numberGroups", "1"));
