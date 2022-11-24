a2f1264fff222ba5ac1a3a1fd43d4a5b893528cf
Killassa <gregory.reymermet@gmail.com>
2022-11-24 11:19:03 +0100

    Add comment Message.java

--- src/main/java/org/example/Message.java
+++ src/main/java/org/example/Message.java
@@ -1,18 +1,46 @@
+/**
+ * Fichier pour gérer un message
+ *
+ * @author Grégory Rey-Mermet
+ * @author Tim Ernst
+ * @author Eric Peronetti
+ * Date    24.11.2022
+ */
 package org.example;
 
 public class Message {
+    /**
+     * Sujet du message
+     */
     private final String subject;
+
+    /**
+     * Contenu du message
+     */
     private final String text;
 
+    /**
+     * Constructeur d'un Message
+     * @param subject   Sujet du message
+     * @param text      Contenu du message
+     */
     public Message(String subject, String text) {
         this.subject = subject;
         this.text = text;
     }
 
+    /**
+     * Permet de récupérer le contenu du message
+     * @return Le contenu du message
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Permet de récupérer le sujet du message
+     * @return Le sujet du message
+     */
     public String getSubject() {
         return subject;
     }
